6d26edd692fbeebe878ca156c352cf42e2c263ed
OpenTelegramFiles <79932684+OpenTelegramFiles@users.noreply.github.com>
2021-03-07 21:50:27 +0100

    Add files via upload

--- dama.php
+++ dama.php
@@ -1,9 +1,9 @@
 <?php
-class damaitaliana {
+ class dama {
  
 	private $ann;
  
-	function __construct( $ann ) {
+	function __construct( $ann = "" ) {
 		$this->ann = $ann;
 		
 	}
@@ -17,34 +17,94 @@ class damaitaliana {
 		return true;
 	}
 	function getChessboard(){
-	    $moves = explode($this->ann, ".");
-	    $temp = "";
+
+	    if(strpos($this->ann, 1, 0) == "."){
+                $this->ann = strpos($this->ann, strlen($this->ann)-1, 1);
+        }
+            $moves = explode( ".",$this->ann);
+
+
+
 	    $turn = "w";
-	    $chessb =  [["b","b","b","b"],["b","b","b","b"],["b","b","b","b"], [" "," "," "," "],[" "," "," "," "],["w","w", "w","w"], ["w", "w","w", "w"],["w","w", "w", "w"]];
+	     $chessb =  [["b","b","b","b"],["b","b","b","b"],["b","b","b","b"], [" "," "," "," "],[" "," "," "," "],["w","w", "w","w"], ["w", "w","w", "w"],["w","w", "w", "w"]];
 	    foreach($moves as $move){
-	        $move3 = explode($move, ",");
-	        $move2 = explode($move3[0],"");
-	        $move5 = explode($move3[1],"");
-	        $temm = $chessb [intval($move2[0])][intval($move2[1])];
+	        $move3 = explode(",",$move);
+	        $move2 = str_split( $move3[0]);
+	        $move5 = str_split( $move3[1]);
+	        $intm21 = intval($move2[1]);
+	        $intm20 = intval($move2[0]);
+	        $intm50 = intval($move5[0]);
+	        $intm51 = intval($move5[1]);
+	        $temm = $chessb [$intm50][$intm51];
+	        if($intm20 == ($intm50 -2) or $intm50 == ($intm20 -2)){//se deve mangiare a sinistra
+	        
+	        if( ($intm21 +1) == $intm51){
 	        if($temm == " "){
-	            $c = intval($move2[1]);
-	            for($i = intval($move2[0]);$i < intval($move5[0]); $i++){
-	                if(intval($move5[1]) > intval($move2[1])){
-	               $chessb [$i -1][$c -1] = " ";
-	            $chessb [$i][$c] = "w";
-	            $c++;
-	            }else{
-	                	               $chessb [$i -1][$c -1] = " ";
-	            $chessb [$i][$c] = "w";
-	            $c--;
-	            }
-	                
-	            }
+	       
+	           
 	            
-	        }else{
+	                $chessb[$intm20][$intm21] = " ";
+	                $chessb[$intm20 + 1][$intm21 - 1] = " ";
+	                $chessb[$intm50][$intm51] = $turn;
+	           	if($turn == "w"){
+	                $turn = "b";
+	   
+	            }else{
+	                $turn = "b";
+	            }
+	           
+	            
+	        }}
+	        if(($intm21 -1) == $intm51){
+	            if($temm == " "){
+	       
+	           
+	            
+	                $chessb[$intm20][$intm21] = " ";
+	                $chessb[$intm20 + 1][$intm21 + 1] = " ";
+	                $chessb[$intm50][$intm51] = $turn;
+	                if($turn == "w"){
+	                $turn = "b";
+	   
+	            }else{
+	                $turn = "b";
+	            }
+	           
+	            
+	        }
+	            // se mangia a destra
+	       } }
+elseif($intm20 == ($intm50 -1)or $intm50 == $intm20 -1){
+	            if($turn =="w"){
+                    if( $intm21 == ($intm51 -1) or $intm21 == $intm51){
+
+                        if($temm == " "){
+
+                            $chessb[$intm20][$intm21] = " ";
+
+                            $chessb[$intm50][$intm51] = $turn;
+                           $turn = "b";
+                        }
+                    }
+
+      }elseif($turn == "b") {
+                    if( $intm21 == ($intm51 +1) or $intm21 == $intm51){
+
+                        if($temm == " "){
+
+                            $chessb[$intm20][$intm21] = " ";
+
+                            $chessb[$intm50][$intm51] = $turn;
+
+                                $turn = "w";
+                        }
+                    }
+
+                }
+	  else{
 	            return "invalid move";
 	        }
-	    }
+	    }}
 	    return $chessb;
 	}
 	
---
+++ exampledama.php
@@ -0,0 +1,5 @@
+<?php
+require_once "dama.php";
+$dama = new dama("52,42");
+print_r($dama->getChessBoard());
+?>
